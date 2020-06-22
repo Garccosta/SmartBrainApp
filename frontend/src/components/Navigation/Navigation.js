a1598e0c1f400c561cae3d76120b7ff8ad5857e1
garccosta <garccosta@gmail.com>
2020-06-21 21:03:36 -0300

    frontend

---
+++ frontend/src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+        
+    if (isSignedIn){
+        return(
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={() => onRouteChange('signOut')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+            </nav>
+        );
+
+        } else {
+            return (
+                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <p onClick={() => onRouteChange('signIn')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                    <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+                </nav>
+            );
+        }
+}
+export default Navigation;
