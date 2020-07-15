23de26c397642f94e132daead2dea248748ea95a
Garccosta <49294653+Garccosta@users.noreply.github.com>
2020-07-15 13:49:17 -0300

    Update knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -6,8 +6,8 @@ module.exports = {
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      user : 'postgres',
-      password : 'postgres',
+      user : '',
+      password : '',
       database : 'smart_brain'
     }, 
     migrations: {
