839d49a80608f0865fff5ded0e4bfbc108031e43
Gabriel Costa <garccosta@gmail.com>
2025-01-02 20:37:56 +0100

    refactor: simplified DB default connection config

--- api/core/knexfile.js
+++ api/core/knexfile.js
@@ -1,50 +1,48 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'pg',
+    client: "postgresql",
     connection: {
-      host : '127.0.0.1',
-      user : '',
-      password : '',
-      database : 'smart_brain'
-    }, 
+      host: "127.0.0.1",
+      user: "user",
+      password: "pass",
+      database: "my_db",
+    },
     migrations: {
-      directory: './migrations'
+      directory: "./migrations",
     },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "user",
+      password: "pass",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "user",
+      password: "pass",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
